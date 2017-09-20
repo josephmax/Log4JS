5d63a38384dc0d48b2c75ee9658dffc4c8da663c
SeanWangx <1971822513@qq.com>
2017-09-20 11:55:42 +0800

    roll back

--- src/util/Log4JS.js
+++ src/util/Log4JS.js
@@ -26,44 +26,36 @@ function Log4JS (lvNm = TYPE.WARN) {
   let level = LEVEL[lvNm]
   console.log(`Log4JS Level: ${lvNm}`)
 
-  Object.keys(TYPE).map(type => {
-    if (!TYPE.hasOwnProperty(type)) return
-    if (type === 'OFF' || type === 'ALL') return
-    Log4JS.prototype[type.toLowerCase()] = function () {
-      if (LEVEL[type] > level) return
-      console[type === 'DEBUG' ? 'log' : type.toLowerCase()](`[${type}]`, ...arguments)
-    }
-  })
   /**
    * 错误级别日志
    */
-  /* Log4JS.prototype.error = function () {
+  Log4JS.prototype.error = function () {
     if (LEVEL[TYPE.ERROR] > level) return
     console.error(`[${TYPE.ERROR}]`, ...arguments)
-  } */
+  }
   /**
    * 警告级别日志
    */
-  /* Log4JS.prototype.warn = function () {
+  Log4JS.prototype.warn = function () {
     if (LEVEL[TYPE.WARN] > level) return
     console.warn(`[${TYPE.WARN}]`, ...arguments)
-  } */
+  }
 
   /**
    * 提示级别日志
    */
-  /* Log4JS.prototype.info = function () {
+  Log4JS.prototype.info = function () {
     if (LEVEL[TYPE.INFO] > level) return
     console.info(`[${TYPE.INFO}]`, ...arguments)
-  } */
+  }
 
   /**
    * 调试级别日志
    */
-  /* Log4JS.prototype.debug = function () {
+  Log4JS.prototype.debug = function () {
     if (LEVEL[TYPE.DEBUG] > level) return
     console.log(`[${TYPE.DEBUG}]`, ...arguments)
-  } */
+  }
 }
 
 export default Log4JS
